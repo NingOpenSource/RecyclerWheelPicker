b809abafa0dfeed5fb161f9d5e8639ccad712c99
Lyon Yan <Lyon1994@users.noreply.github.com>
2018-06-15 17:34:40 +0800

    Update RecyclerWheelPicker.java

--- RecyclerWheelPicker.java
+++ RecyclerWheelPicker.java
@@ -14,8 +14,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import com.gzleihou.oolagongyi.util.DensityUtil;
-
 public class RecyclerWheelPicker<T> extends RecyclerView {
     public RecyclerWheelPicker(Context context) {
         super(context);
