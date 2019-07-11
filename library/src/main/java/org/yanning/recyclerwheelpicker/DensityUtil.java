46a4fbd16dfc7435a6469594cbb2b60c34787dc7
Ning1994 <765211630@qq.com>
2019-07-11 16:29:58 +0800

    升级gradle插件，尝试修复与bottomsheet的滑动bug

--- library/src/main/java/org/yanning/recyclerwheelpicker/DensityUtil.java
+++ library/src/main/java/org/yanning/recyclerwheelpicker/DensityUtil.java
@@ -1,6 +1,7 @@
 package org.yanning.recyclerwheelpicker;
 
 import android.content.Context;
+import android.content.res.Resources;
 
 class DensityUtil {
 
@@ -17,6 +18,23 @@ class DensityUtil {
      */
     public static int px2dip(Context context, float pxValue) {
         final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
+    }
+
+
+    /**
+     * 根据手机的分辨率从 dp 的单位 转成为 px(像素)
+     */
+    public static int dip2px(float dpValue) {
+        final float scale = Resources.getSystem().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+
+    /**
+     * 根据手机的分辨率从 px(像素) 的单位 转成为 dp
+     */
+    public static int px2dip(float pxValue) {
+        final float scale = Resources.getSystem().getDisplayMetrics().density;
         return (int) (pxValue / scale + 0.5f);
     }
 }  
